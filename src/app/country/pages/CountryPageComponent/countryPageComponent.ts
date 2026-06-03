cb92a150010f9d37d7552a974834c252e7af0812
RobertoRB <robertorodriguezbqz@gmail.com>
2026-06-03 16:42:58 -0400

    wip: start country detail page (informacion del pais)
    WIP: scaffolding for /country/by/:code detail view. Not finished yet.
    
    - feat(country.service): add searchPaisByInformacion(code) -> GET /alpha/{code}
    - feat(country): add CountryPageComponent wired to ActivatedRoute + rxResource
    - feat(shared): add minimal NotFound component for error/empty state
    - template currently shows only country name; flag, capital, population,
      region/subregion, borders still pending
    - TODO before merging: wire 'Más información' in CountryList to detail route
    - TODO before merging: fix 'Volver' link (currently /countries, should be /country)
    - TODO before merging: fix NG8107 redundant optional chain in detail template
    - TODO before merging: remove unused DecimalPipe import or use it for population

--- src/app/country/pages/CountryPageComponent/countryPageComponent.ts
+++ src/app/country/pages/CountryPageComponent/countryPageComponent.ts
@@ -1,8 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
+import { NotFound } from '../../../shared/components/not-found/not-found';
 
-import { Component } from '@angular/core';
 @Component({
   selector: 'app-country-page-component',
-  imports: [],
+  imports: [DecimalPipe, NotFound],
   templateUrl: './countryPageComponent.html',
+
 })
-export class countryPageComponent { }
+export class countryPageComponent {
+
+  countryCode = inject(ActivatedRoute).snapshot.paramMap.get('code');
+  countryService = inject(CountryService);
+
+  countryResource = rxResource ({
+    params: () => ({ code: this.countryCode}),
+    stream: ({ params }) => {
+      return this.countryService.searchPaisByInformacion(params.code!);
+    }
+  })
+
+ }
