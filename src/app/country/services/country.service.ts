ea5f0b7d5892fb999c8a1c2fa06756e5d996af8f
RobertoRB <robertorodriguezbqz@gmail.com>
2026-06-10 15:23:21 -0400

    implementación de caché para búsquedas de capital, país y región

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -12,12 +12,23 @@ const URL_API = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCachePais = new Map<string, Country[]>();
+   private queryCacheRegion = new Map<string, Country[]>();
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
+
+    if (this.queryCacheCapital.has(query)){
+      return of (this.queryCacheCapital.get(query)?? []);
+    }
+
+
+
    
     return this.http.get<RESTCountry[]>(`${URL_API}/capital/${query}`).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((resp) => this.queryCacheCapital.set(query, resp)),
       catchError((err) => {
         return of([]);
       }),
@@ -27,6 +38,8 @@ export class CountryService {
   searchByRegion(region: string): Observable<Country[]> {
     return this.http.get<RESTCountry[]>(`${URL_API}/region/${region}`).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((resp) => this.queryCacheRegion.set(region, resp)),
+
       catchError((err) => {
         return throwError(() => new Error('No se encontro datos con esa region'));
       }),
@@ -34,8 +47,13 @@ export class CountryService {
   }
 
   searchByPais(pais: string): Observable<Country[]> {
+    if (this.queryCachePais.has(pais)){
+      return of (this.queryCachePais.get(pais)?? []);
+    }
+
     return this.http.get<RESTCountry[]>(`${URL_API}/name/${pais}`).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((resp) => this.queryCachePais.set(pais, resp)),
       catchError((err) => {
         return of([]);
       }),
